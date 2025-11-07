ea20459aa3d32e6c887b250b94962a70e5b9a5e3
Vivaan Pandey <shivikashivikaaa@gmail.com>
2025-11-07 18:59:21 +0530

    euler integration method - simple first order

---
+++ euler.js
@@ -0,0 +1,32 @@
+// Euler integration - first order method
+// Fast but energy drifts over time
+
+
+function integrate_euler(state,dt){
+const alphas=computeAlphas(state)
+
+state.theta1+=state.omega1*dt
+state.theta2+=state.omega2*dt
+state.omega1+=alphas.alpha1*dt
+state.omega2+=alphas.alpha2*dt
+}
+
+
+
+
+function integrate_euler_step(state,dt){
+const {alpha1,alpha2}=computeAlphas(state)
+
+const newState={
+theta1:state.theta1+state.omega1*dt,
+theta2:state.theta2+state.omega2*dt,
+omega1:state.omega1+alpha1*dt,
+omega2:state.omega2+alpha2*dt,
+m1:state.m1,
+m2:state.m2,
+L1:state.L1,
+L2:state.L2
+}
+
+return newState
+}
