3cf9bcd9d18c747578ac2ac5cdacbfc28a74e231
Vivaan Pandey <shivikashivikaaa@gmail.com>
2025-11-07 20:58:53 +0530

    added the chaos background which i talked bout, there are as of now 50 colliding pendulums with varied initial conditions, added large dt for sm jittery motion and onsquare collision detection, with ghostly trail effect using fadecanvas with apha near like .05, also added a small fps counter with batched rendering to run in my pc

--- canvas-utils.js
+++ canvas-utils.js
@@ -1,5 +1,7 @@
-// Canvas rendering utilities
-// Optimized for low-power systems
+
+
+// canvas rendering utilities
+
 
 
 function drawPendulum(ctx,state,centerX,centerY){
@@ -9,11 +11,14 @@ state.L1,state.L2,
 centerX,centerY
 )
 
+
+
 ctx.strokeStyle='#fff'
 ctx.lineWidth=2
 ctx.beginPath()
 ctx.moveTo(centerX|0,centerY|0)
 ctx.lineTo(x1|0,y1|0)
+
 ctx.lineTo(x2|0,y2|0)
 ctx.stroke()
 
@@ -24,6 +29,8 @@ ctx.arc(x1|0,y1|0,5,0,Math.PI*2)
 ctx.fill()
 
 ctx.beginPath()
+
+
 ctx.arc(x2|0,y2|0,8,0,Math.PI*2)
 ctx.fill()
 
@@ -35,18 +42,26 @@ return {x1,y1,x2,y2}
 
 function drawPendulumBatch(ctx,pendulums,centerX,centerY){
 ctx.strokeStyle='#fff'
+
+
 ctx.lineWidth=1
 ctx.beginPath()
 
 for(let p of pendulums){
 const {x1,y1,x2,y2}=polarToCartesian(
 p.theta1,p.theta2,
+
+
+
 p.L1,p.L2,
 centerX,centerY
 )
 
 ctx.moveTo(centerX|0,centerY|0)
 ctx.lineTo(x1|0,y1|0)
+
+
+
 ctx.lineTo(x2|0,y2|0)
 }
 
@@ -56,12 +71,21 @@ ctx.stroke()
 ctx.fillStyle='#fff'
 ctx.beginPath()
 for(let p of pendulums){
+
+
+
 const {x1,y1,x2,y2}=polarToCartesian(
 p.theta1,p.theta2,
+
+
+
 p.L1,p.L2,
 centerX,centerY
 )
 ctx.moveTo(x2|0,y2|0)
+
+
+
 ctx.arc(x2|0,y2|0,3,0,Math.PI*2)
 }
 ctx.fill()
@@ -72,6 +96,9 @@ ctx.fill()
 
 function drawTrail(ctx,x,y,color){
 ctx.fillStyle=color||'#fff'
+
+
+
 ctx.fillRect(x|0,y|0,1,1)
 }
 
@@ -79,7 +106,10 @@ ctx.fillRect(x|0,y|0,1,1)
 
 
 function clearCanvas(ctx,width,height){
+
 ctx.fillStyle='#000'
+
+
 ctx.fillRect(0,0,width,height)
 }
 
@@ -87,5 +117,7 @@ ctx.fillRect(0,0,width,height)
 
 function fadeCanvas(ctx,width,height,alpha){
 ctx.fillStyle=`rgba(0,0,0,${alpha})`
+
+
 ctx.fillRect(0,0,width,height)
 }
